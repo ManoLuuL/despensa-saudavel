f7dc53674710fa2a5a004b82f79210979cf42b6a
luis.couto <luis.couto@frgnet.com.br>
2023-03-31 07:42:05 -0300

    fix: ajustado styles para stylescomponent

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -1,54 +1,60 @@
 import { FC } from "react";
 import { NavLink } from "react-router-dom";
-import { ContainerHome } from "./styles";
+import {
+  Buttons,
+  ContainerGlobalHome,
+  ContainerHome,
+  ContainerImg,
+  Description,
+  Overlay,
+  TitlePage,
+} from "./styles";
 import imgLogo from "./assets/logo_facul.png";
 import imgHome from "./assets/image.png";
 
 const HomePage: FC = () => {
   return (
-    <ContainerHome>
-      <div className="home-page">
-        <div className="containerHome">
-          <div className="overlay">
-            <img
-              src={imgLogo}
-              alt=""
-              style={{
-                height: "5.5rem",
-              }}
-            />
-            <div>
-              <h1 className="h1_homepage">Despensa </h1>
-              <h1 className="h1_homepage">Saudável</h1>
-            </div>
-            <span className="description">
-              Aqui você pode criar refeições saudáveis com base no que já tem em
-              casa. Esqueça a necessidade de ir às compras toda vez que quiser
-              fazer uma refeição saudável. Nossa plataforma é fácil de usar e
-              oferece opções deliciosas e nutritivas para todas as refeições do
-              dia.
-            </span>
-            <div className="buttons">
-              <NavLink to={"/Login"}>
-                <button className="button">Login</button>
-              </NavLink>
-              <NavLink to={"/Registrar"}>
-                <button className="button">Registrar-se</button>
-              </NavLink>
-            </div>
+    <ContainerGlobalHome>
+      <ContainerHome>
+        <Overlay>
+          <img
+            src={imgLogo}
+            alt=""
+            style={{
+              height: "5.5rem",
+            }}
+          />
+          <div>
+            <TitlePage>Despensa </TitlePage>
+            <TitlePage>Saudável</TitlePage>
           </div>
-          <div className="rightImg">
-            <img
-              src={imgHome}
-              alt=""
-              style={{
-                height: "50rem",
-              }}
-            />
-          </div>
-        </div>
-      </div>
-    </ContainerHome>
+          <Description>
+            Aqui você pode criar refeições saudáveis com base no que já tem em
+            casa. Esqueça a necessidade de ir às compras toda vez que quiser
+            fazer uma refeição saudável. Nossa plataforma é fácil de usar e
+            oferece opções deliciosas e nutritivas para todas as refeições do
+            dia.
+          </Description>
+          <Buttons>
+            <NavLink to={"/Login"}>
+              <button className="button">Login</button>
+            </NavLink>
+            <NavLink to={"/Registrar"}>
+              <button className="button">Registrar-se</button>
+            </NavLink>
+          </Buttons>
+        </Overlay>
+        <ContainerImg>
+          <img
+            src={imgHome}
+            alt=""
+            style={{
+              height: "50rem",
+            }}
+          />
+        </ContainerImg>
+      </ContainerHome>
+    </ContainerGlobalHome>
   );
 };
 
--- src/pages/home-page/styles.ts
+++ src/pages/home-page/styles.ts
@@ -1,48 +1,54 @@
 import styled from "styled-components";
 
+export const ContainerGlobalHome = styled.div`
+  height: 100vh;
+  background-color: black;
+`;
+
+export const Overlay = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  text-align: left;
+  max-width: 52vw;
+`;
+
+export const TitlePage = styled.h1`
+  font-size: 6rem;
+  color: white;
+  margin: 0;
+  padding: 0;
+  display: flex;
+`;
+
+export const Description = styled.span`
+  text-align: justify;
+  font-size: 1.5rem;
+`;
+
+export const ContainerImg = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 export const ContainerHome = styled.div`
-  .home-page {
-    height: 100vh;
-    background-color: black;
-  }
+  display: flex;
+  justify-content: space-between;
+  height: 100%;
+  padding: 30px;
+`;
 
-  .containerHome {
-    display: flex;
-    justify-content: space-between;
-    height: 100%;
-    padding: 30px;
-  }
-  .overlay {
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: flex-start;
-    text-align: left;
-    max-width: 52vw;
-  }
-
-  .h1_homepage {
-    font-size: 6rem;
-    color: white;
-    margin: 0;
-    padding: 0;
-    display: flex;
-  }
-
-  .description {
-    text-align: justify;
-    font-size: 1.5rem;
-  }
-
-  .buttons {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
+export const Buttons = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   button {
     margin: 0 1rem;
+    width: 250px;
     padding: 0.75rem 1.5rem;
     font-size: 1.25rem;
     border-radius: 0.5rem;
@@ -54,11 +60,5 @@ export const ContainerHome = styled.div`
     :hover {
       background-color: #8000ff;
     }
-  }
-
-  .rightImg {
-    display: flex;
-    align-items: center;
-    justify-content: center;
   }
 `;
