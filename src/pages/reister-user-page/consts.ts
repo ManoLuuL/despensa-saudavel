95ffc8e013ad5b4a9d7376f1c5b7ce4f245df227
Luis <ticocouto_14@hotmail.com>
2023-04-23 12:42:35 -0300

    feat: adicionado novos campos no registro

---
+++ src/pages/reister-user-page/consts.ts
@@ -0,0 +1,16 @@
+import { UserSex } from "./types";
+
+export const ITENS_USER_SEX: UserSex[] = [
+  {
+    id: 1,
+    description: "Feminino",
+  },
+  {
+    id: 2,
+    description: "Masculino",
+  },
+  {
+    id: 3,
+    description: "Outros",
+  },
+];
--- src/pages/reister-user-page/types.ts
+++ src/pages/reister-user-page/types.ts
@@ -1,4 +1,12 @@
-export type LoginFormType = {
+export type UserSex = {
+  id: number;
+  description: string;
+};
+
+export type LoginForm = {
+  name: string;
   email: string;
   password: string;
+  confirmPassword: string;
+  userSex: UserSex;
 };
