0ae1a4bbf4ead1a9f51c888896d1d03a5ef96c39
luis.couto <luis.couto@frgnet.com.br>
2023-06-07 13:11:32 -0300

    fix(about): modificado informações sobre o projeto

--- src/pages/about/about-page.tsx
+++ src/pages/about/about-page.tsx
@@ -11,9 +11,38 @@ export const AboutPage = () => {
           <>
             <h3 className="flex justify-content-center">Sobre o projeto</h3>
             <Divider />
+            <p>
+              Este projeto de Desenvolvimento de Website de busca de receitas
+              por ingredientes é resultado de um trabalho de conclusão de curso
+              (TCC) que busca unir tecnologia, nutrição e praticidade. A
+              proposta é desenvolver um website que permita aos usuários
+              encontrarem receitas personalizadas com base nos ingredientes
+              disponíveis em sua despensa. Para isso, será implementado um banco
+              de dados para armazenar as receitas, um algoritmo de satisfação
+              para oferecer sugestões relevantes, um vínculo com a área de
+              nutrição para fornecer informações nutricionais e um cálculo do
+              Índice de Massa Corporal (IMC) para auxiliar na promoção de uma
+              alimentação saudável.
+            </p>
+            <p>
+              Em resumo, o projeto tem como objetivo fornecer uma plataforma
+              online que simplifique a busca por receitas personalizadas, unindo
+              tecnologia, nutrição e praticidade. O uso de um banco de dados,
+              algoritmo de satisfação, vínculo com a área de nutrição e cálculo
+              do IMC tornam esse projeto abrangente e relevante para promover
+              uma alimentação equilibrada e saudável.
+            </p>
+            <Divider />
             <p>Desenvolvedores:</p>
             <p>Luis Ricardo C. Couto</p>
             <p>Vitor Molina Correia do Nascimento</p>
+            <Divider />
+            <h3>Tecnologias Utilizadas</h3>
+            <p>
+              Este website será desenvolvido utilizando as seguintes
+              tecnologias: Front-end: HTML, CSS, TypeScript e React Back-end:
+              indefinido Funcionalidades do Website
+            </p>
           </>
         }
       />
