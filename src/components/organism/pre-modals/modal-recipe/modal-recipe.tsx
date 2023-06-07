1bf832c8e29e9c237cdc80e2582cda0d846e6815
luis.couto <luis.couto@frgnet.com.br>
2023-06-07 08:51:42 -0300

    fix: adicionado botão de favoritar/desfavoritar receitas

--- src/components/organism/pre-modals/modal-recipe/modal-recipe.tsx
+++ src/components/organism/pre-modals/modal-recipe/modal-recipe.tsx
@@ -1,12 +1,16 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { RecipesModalProps } from "./types";
 import { Divider } from "primereact/divider";
 import { Modal } from "../../modal";
+import { Button } from "../../../molecules/button-custom";
+import { useToast } from "../../../../globals/hooks/use-toast";
 
 export const RecipesModal: FC<RecipesModalProps> = (props) => {
   const { recipes, onHide } = props;
 
   const modoPreparo = recipes?.modo_preparo.split("\n") ?? [];
+  const [favorite, setFavorite] = useState(false);
+  const { showSuccess } = useToast();
 
   return (
     <Modal
@@ -14,6 +18,21 @@ export const RecipesModal: FC<RecipesModalProps> = (props) => {
       title={recipes?.titulo}
       width={{ default: "60vw", mobile: "80vw" }}
     >
+      <Button
+        text={!favorite ? "Favoritar Receita" : "Desfavoritar Receita"}
+        icon={{
+          name: "star",
+          size: 18,
+        }}
+        color={favorite ? "alert" : "secondary"}
+        onClick={() => {
+          setFavorite(!favorite);
+          if (favorite) showSuccess("Receita removida dos favoritos");
+          else showSuccess("Receita favoritada com sucesso");
+        }}
+      />
+
+      <Divider />
       <h2>Ingredientes:</h2>
       {recipes?.ingredientes.map((itens, index) => (
         <p key={index}>
